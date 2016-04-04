98d33309498fd9bd60397747a3e828b0a1cfde54
HarryWhewell <harry.whewell@myfuturesbright.co.uk>
2016-04-04 20:01:31 +0100

    Adding basic structure

---
+++ backend/controllers/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * User: harry
+ * Date: 04/04/2016
+ * Time: 19:40
+ */
+class AuthController
+{
+
+}
---
+++ backend/controllers/BlogPostController.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * User: harry
+ * Date: 04/04/2016
+ * Time: 19:41
+ */
+class BlogPostController
+{
+
+}
