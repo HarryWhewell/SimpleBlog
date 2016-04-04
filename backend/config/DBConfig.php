2ea1bea31df642f60d3d783d74d75dfac28958b0
HarryWhewell <harry.whewell@myfuturesbright.co.uk>
2016-04-04 23:37:09 +0100

    Adding dbconfig and register function

--- backend/config/DBConfig.php
+++ backend/config/DBConfig.php
@@ -5,7 +5,35 @@
  * Date: 04/04/2016
  * Time: 19:41
  */
-class DBConfig
-{
+namespace backend\config\DBConfig;
+
+// use classes what we need;
+use PDO;
+use PDOException;
+
+class DBConfig{
+
+    // Declare connection variables
+    private $db_host = 'localhost';
+    private $db_user = 'harry';
+    private $db_pass = 'password';
+    private $db_name = 'simple_blog_post';
+    public $conn;
+
+    public function connect(){
+        try{
+            // Make reference to PDO and pass in connection params
+            $this->conn = new PDO("mysql:host={$this->db_host};dbname={$this->db_name}",$this->db_user,$this->db_pass);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Set Error Attributes
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $this->conn;
+    }
+    public function disconnect(){
+        //Destroy PDO object by ensuring $conn reference to it is null
+        $this->conn = null;
+    }
 
 }
--- backend/controllers/AuthController.php
+++ backend/controllers/AuthController.php
@@ -5,7 +5,44 @@
  * Date: 04/04/2016
  * Time: 19:40
  */
+namespace backend\controllers\AuthController;
+
+// use classes what we need;
+use backend\config\DBConfig\DBConfig;
+use PDOException;
+
 class AuthController
 {
+    private $connect;
 
+    function __construct()
+    {
+        $dbConfig = new DBConfig();
+        $this->connect = $dbConfig->connect();
+    }
+
+    function registerUser($username, $password){
+        try{
+            $stmt = $this->connect->prepare("INSERT INTO users(username,password) VALUES(:username, :password)");
+            $stmt->bindparam(":username",$username);
+            $stmt->bindparam(":password",$password);
+            $stmt->execute();
+            return true;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    function loginUser($username, $password){
+
+    }
+
+    function getUserById($userId){
+
+    }
+
+    function resetPassword($oldPassword, $newPassword){
+
+    }
 }
